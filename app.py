0946e2c3ea994b64163b3ab0094deb3c6c754663
Natnaelsamuel <natisami907@gmail.com>
2025-11-17 16:30:00 +0300

    Finished buildung the menu and created the other functions

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
+
+def AddTransactions():
+    print("\n")
+    print("Adding Transactions...")
+    print("\n")
+    Menu()
+
+def ViewTransactions():
+    print("\n")
+    print("Viewing Transactions...")
+    print("\n")
+    Menu()
+
+def ViewSummary():
+    print("\n")
+    print("Viewing Summary...")
+    print("\n")
+    Menu()
+
 def Menu():
     print("=== Personal Budget Tracker ===")
     print("1. Add Transactions")
@@ -5,5 +24,19 @@ def Menu():
     print("3. View Summary")
     print("4. Exit")
     choice = input("Choose an option (1-4): ")
+
+    if choice == '1':
+        AddTransactions()
+    elif choice == '2':
+        ViewTransactions()
+    elif choice == '3':
+        ViewSummary()
+    elif choice == '4':
+        print("Exiting the app")
+        return
+    else:
+        print("Invalid choice. Please try again.")
+        Menu();
+
 
 Menu()
