db967693ddaf13398dd1e78abcc00ae211097e56
Natnaelsamuel <natisami907@gmail.com>
2025-11-17 16:21:45 +0300

    Built the main menu

---
+++ app.py
@@ -0,0 +1,9 @@
+def Menu():
+    print("=== Personal Budget Tracker ===")
+    print("1. Add Transactions")
+    print("2. View Transactions")
+    print("3. View Summary")
+    print("4. Exit")
+    choice = input("Choose an option (1-4): ")
+
+Menu()
